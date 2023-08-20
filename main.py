aa5a1695876b1701f04592a7b4b2d30efe56df36
AashirSiddiqui <aashir.m.siddiqui@gmail.com>
2023-08-19 19:19:50 -0700

    renamed:    templates/style.css -> static/style.css Put style.css in static and got it working. Started equations.

--- main.py
+++ main.py
@@ -10,10 +10,6 @@ def mainpage():
 def lessonpage():
     return render_template("lesson.html", lesson=["ID01Learntest", "ID02", "ID00Welcome to this text","ID03Try rereading the text.", "ID00You will learn here"])
 
-@app.route("/style.css")
-def css():
-    return render_template("style.css")
-
 app.debug = True 
 
 if __name__ == "__main__":

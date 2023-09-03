6e6ed3a18890a0fadc934999089de6baacda94bd
AashirSiddiqui <aashir.m.siddiqui@gmail.com>
2023-09-03 11:28:26 -0700

    modified:   main.py 	modified:   static/style.css 	renamed:    static/account.html -> templates/account.html 	modified:   templates/index.html 	modified:   templates/lesson.html 	new file:   templates/redirect.html Created a redirect page, added Lesson Completed widget.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import json
 
 app = Flask((__name__))
 
@@ -6,9 +7,21 @@ app = Flask((__name__))
 def mainpage():
     return render_template("index.html")
 
+@app.route("/home")
+def accountpage():
+    return render_template("account.html")
+
 @app.route("/lesson.html")
+def redirectToLesson():
+    return render_template("redirect.html", page="lesson")
+
+@app.route("/lesson")
 def lessonpage():
     return render_template("lesson.html", lesson=["ID01Learntest", "ID02", "ID00Welcome to this text","ID03Try rereading the text.", "ID00You will learn here"])
+
+@app.route("/submitlesson", methods=["POST", "GET"])
+def submitlesson():
+    return json.dumps("hello")
 
 app.debug = True 
 

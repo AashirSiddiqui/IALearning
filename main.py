94ca9bed0a77c9758b46d99226e96a779a907752
AashirSiddiqui <aashir.m.siddiqui@gmail.com>
2023-09-16 18:11:37 -0700

    Basic signup page.

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
-import pymongo
-import json
+from flask import Flask, render_template, request, session
+from pymongo import MongoClient
+import json, os
+
+os.environ['MONGODB_URI'] = "mongodb://ialearning:programmer-of-databases-MGDB@cluster0-shard-00-00.dc1ys.mongodb.net:27017,cluster0-shard-00-01.dc1ys.mongodb.net:27017,cluster0-shard-00-02.dc1ys.mongodb.net:27017/?ssl=true&replicaSet=atlas-54vvqc-shard-0&authSource=admin&retryWrites=true&w=majority" # only if not deployed
+
+client = MongoClient(os.getenv("MONGODB_URI"))
+db = client.IALearning
+
+accounts = db.Accounts
+lessons = db.Lessons
+
+print(db.list_collection_names())
 
 app = Flask((__name__))
 
@@ -11,6 +21,21 @@ def mainpage():
 @app.route("/home")
 def accountpage():
     return render_template("account.html")
+
+@app.route("/signup", methods=["POST", "GET"])
+def signuppage():
+    if request.method == "GET":
+        return render_template("signup.html")
+    else:
+        username = request.form["username"]
+        password = request.form["password"]
+        if accounts.count_documents({"username":username}, limit = 1) > 0:
+            print("exists")
+        else:
+            accounts.insert_one({"username":username, "password":password})
+        # return render_template("account-created.html")
+        session["username"] = username
+        return "200"
 
 @app.route("/lesson.html")
 def redirectToLesson():
@@ -23,6 +48,7 @@ def lessonpage():
 @app.route("/submitlesson", methods=["POST", "GET"])
 def submitlesson():
     return json.dumps("hello")
+
 
 app.debug = True 
 
